6079275c7ef50d6f3890485b78e3634d6ea5481f
yuvaraj <yuvaraj091985@gmail.com>
2020-06-21 01:39:24 -0400

    - Modified the files with respect to requirement as Element and Item

--- app/src/main/java/com/yuvaraj/coding/songtrack/navigation/ElementListViewHolder.java
+++ app/src/main/java/com/yuvaraj/coding/songtrack/navigation/ElementListViewHolder.java
@@ -1,6 +1,7 @@
 package com.yuvaraj.coding.songtrack.navigation;
 
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,20 +10,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.yuvaraj.coding.songtrack.R;
 
 class ElementListViewHolder extends RecyclerView.ViewHolder {
-    private TextView textView;
+    public TextView textView;
+    public LinearLayout outerLayout;
 
     public ElementListViewHolder(@NonNull View itemView) {
         super(itemView);
         textView = itemView.findViewById(R.id.name);
+        outerLayout = itemView.findViewById(R.id.element_layout);
     }
 
-    public void bind(final String value, final ElementListAdapter.OnItemClickListener itemClicklistener) {
+    public void bind(final String value) {
         textView.setText(value);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                itemClicklistener.onItemClick(value);
-            }
-        });
     }
 }
